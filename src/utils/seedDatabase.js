cfca93b819c86b16533543f60770b03eaa1c7cd5
Sebastian-Paulluk <129323586+Sebastian-Paulluk@users.noreply.github.com>
2024-11-06 21:09:06 -0300

    Se crean carpetas

---
+++ src/utils/seedDatabase.js
@@ -0,0 +1,14 @@
+const Autor = require('../models/autor.model')
+const Libro = require('../models/libro.model')
+
+async function seedDatabase() {
+    try {
+        
+
+    } catch(error) {
+
+
+    }
+}
+
+module.exports = seedDatabase
