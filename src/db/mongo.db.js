9c492aad1c0f96d17082fa740204ac10518ae1eb
Sebastian-Paulluk <129323586+Sebastian-Paulluk@users.noreply.github.com>
2024-11-11 10:02:36 -0300

    Modifico url de conexion a mongodb

--- src/db/mongo.db.js
+++ src/db/mongo.db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const mongoUrl = process.env.MONGO_URL || 'mongodb://admin:123456@localhost:27017/';
 
 async function connectToDatabase() {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DB_NAME})
+        await mongoose.connect(mongoUrl, {dbName: process.env.MONGO_DB_NAME})
         console.log('Conexión a mongo con éxito');
     } catch (error) {
         console.error('Error al conectarse a mongo', error);
